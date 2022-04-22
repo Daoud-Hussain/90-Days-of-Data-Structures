c4f17acf2b91e8c6b508bf44b9915de266d902e1
Daoud-Hussain <daoudhussain302@gmail.com>
2022-04-22 00:23:19 -0700

    Updating Queue Implementation

--- Queue/simpleQueue.cpp
+++ Queue/simpleQueue.cpp
@@ -10,6 +10,7 @@ struct Queue
     int *arr = new int[size];
 
 
+    //Function to add an element in the queue
     void enQueue(int value)
     {
         if (front == -1 && rear == -1)
@@ -24,6 +25,19 @@ struct Queue
         }
     }
 
+
+    //Function to remove an element from the queue
+    int deQueue()
+{
+    if (!isEmpty())
+    {
+       int value = arr[front];
+       front++;
+       return value;
+    }
+}
+
+    //Funtion to check whether the queue is empty or not
     int isEmpty(){
         if (front == -1 && rear == -1){
             return 1;
@@ -31,6 +45,8 @@ struct Queue
         return 0;
     }
 
+
+    //Funtion to check whether the queue is FULL or not
     int isFull(){
         if (front == 0 && rear == size - 1)
         {
@@ -39,6 +55,8 @@ struct Queue
         return 0;
     }
 };
+
+
 int main()
 {
     Queue q;
@@ -46,7 +64,9 @@ int main()
     q.enQueue(20);
     q.enQueue(30);
     q.enQueue(40);
-    q.enQueue(50);
+    q.deQueue();
+    q.deQueue();
+    q.deQueue();
     
 
     return 0;
