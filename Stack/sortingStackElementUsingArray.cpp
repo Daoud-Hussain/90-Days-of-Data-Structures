91602d05714c0b3f583301decc089d2eaee7d41d
Daoud-Hussain <daoudhussain302@gmail.com>
2022-04-29 11:43:07 -0700

    Adding Stack Sorting Using Array

---
+++ Stack/sortingStackElementUsingArray.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+struct Stack
+{
+    int top = -1;
+    int size = 10;
+    int *arr = new int[size];
+
+    bool isFull()
+    {
+        if (top == size - 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void push(int value)
+    {
+        if (isFull())
+        {
+            cout << "Stack UnderFLow!!" << endl;
+        }
+        else
+        {
+            top++;
+            arr[top] = value;
+        }
+    }
+
+    int pop()
+    {
+
+        if (isEmpty())
+        {
+            cout << "Stack UnderFlow";
+            return -1;
+        }
+        else
+        {
+            int value = arr[top];
+            top--;
+            return value;
+
+        }
+    }
+    void display()
+    {
+        int t = top;
+        while (t != -1)
+        {
+            cout << arr[t] << " " ;
+            t--;
+        }
+    }
+};
