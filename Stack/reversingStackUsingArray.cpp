d5167dd4aa68e0e0bbbb9d4e7a40cebee816265c
Daoud-Hussain <daoudhussain302@gmail.com>
2022-05-06 22:06:10 -0700

    Reversing a stack using Array

---
+++ Stack/reversingStackUsingArray.cpp
@@ -0,0 +1,72 @@
+/*
+Problem Statement:
+Reverse a Given stack using an additional array.
+You can't use anyother stack to implement.
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Stack
+{
+    int top = -1;
+    int size = 10;
+    int *arr = new int[size];
+
+    bool isFull()
+    {
+        if (top == size - 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void push(int value)
+    {
+        if (isFull())
+        {
+            cout << "Stack UnderFLow!!" << endl;
+        }
+        else
+        {
+            top++;
+            arr[top] = value;
+        }
+    }
+
+    int pop()
+    {
+
+        if (isEmpty())
+        {
+            cout << "Stack UnderFlow";
+            return -1;
+        }
+        else
+        {
+            int value = arr[top];
+            top--;
+            return value;
+
+        }
+    }
+    void display()
+    {
+        int t = top;
+        while (t != -1)
+        {
+            cout << arr[t] << " " ;
+            t--;
+        }
+    }
+};
