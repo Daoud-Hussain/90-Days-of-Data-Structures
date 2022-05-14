7954e8f1fc9408bde6c426200aeb83ca8191e8cc
Daoud-Hussain <daoudhussain302@gmail.com>
2022-05-14 03:24:37 -0700

    Updating tree Practice tasks

---
+++ DSA Practise/inOrderTraversalPRactice.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct Tree {
+    Tree* left;
+    Tree* right;
+    int data;
+};
+Tree* root; 
+
+
+Tree* createTreeNode(int value){
+    Tree* curr = new Tree();
+    if(curr == root){
+        curr->left = NULL;
+        curr->right = NULL;        
+        curr->data = value;
+        root = curr;
+
+    }
+    else{
+        curr->left = NULL;
+        curr->right = NULL;        
+        curr->data = value;
+    }
+}
+
+
+//InOrder Traversal using Recursion
+void inOrderTraversal(Tree* t){
+    if(t == NULL){
+        return;
+    }
+    inOrderTraversal(t->left);
+    cout<<t->data<<" ";
+    inOrderTraversal(t->right);
+}
+
+int main(){
+    Tree* parent = createTreeNode(20);
+    parent->left = createTreeNode(30);
+    parent->right = createTreeNode(40);
+    parent->left->left = createTreeNode(70);
+    parent->left->right = createTreeNode(50);
+    parent->right->left = createTreeNode(10);
+    parent->right->right = createTreeNode(50);
+
+    inOrderTraversal(parent);
+
+}
