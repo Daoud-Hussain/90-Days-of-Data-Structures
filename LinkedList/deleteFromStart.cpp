6b6b9fbe9d5bb93b3e9cc59dd77b36c4a04c3dca
Daoud-Hussain <daoudhussain302@gmail.com>
2022-05-23 00:24:18 -0700

    Deleting a node from the start of LL

---
+++ LinkedList/deleteFromStart.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+Node* first;
+Node* last;
+
+//Travesing/Printing nodes of linkedlist
+void traverseNodes(Node* givenFirstNode){
+    while(givenFirstNode != NULL){
+        cout<<givenFirstNode->data<<" ";
+        givenFirstNode = givenFirstNode->next;
+    }
+}
+
+//Inserting a node at the end of linkedlist
+Node* insertAtEnd(int value){
+    if(first == NULL && last == NULL){
+        Node* curr = new Node();
+        curr->data = value;
+        curr->next = NULL;
+        last = curr;
+        first = curr;
+    }
+    else{
+        Node* curr = new Node();
+        curr->data = value;
+        curr->next = NULL;
+        last->next = curr;
+        last = curr;
+    }
+}
+ 
+
+
+int main(){
+
+    insertAtEnd(10);
+    insertAtEnd(20);
+    insertAtEnd(30);
+    insertAtEnd(40);
+    insertAtEnd(50);
+
+    traverseNodes(first);
+
+}
