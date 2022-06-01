7f3ce73494d4d509cf72c060f29f38fb5a212d66
Daoud-Hussain <daoudhussain302@gmail.com>
2022-06-01 09:36:48 +0500

    Deleting Nodes from doubly linkedlist

---
+++ Double Linkedlist/deletingANodeFromStart.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node* prev;
+    Node* next;
+};
+Node* first;
+Node* last;
+
+void insertAtEnd(int nodeData){
+    Node* curr = new Node();
+
+    //Checking empty condition
+    if(first == NULL && last == NULL){
+        curr->data = nodeData;
+        curr->prev = NULL;
+        curr->next = NULL;
+        first = curr;
+        last = curr;
+    }
+    else{
+        curr->data = nodeData;
+        curr->next = NULL;
+        curr->prev = last;
+        last->next = curr;
+        last = curr;
+    }
+
+}
+
+//Travesing/Printing nodes of linkedlist
+void traverseNodes(){
+    Node* givenFirstNode = first;
+    while(givenFirstNode != NULL){
+        cout<<givenFirstNode->data<<" ";
+        givenFirstNode = givenFirstNode->next;
+    }
+}
+
+void deleteANodeFromStart(){
+    Node* temp = first;
+    first = first->next;
+    temp->next = NULL;
+    first->prev = NULL;
+    delete temp;
+}
+
+int main()
+{
+    insertAtEnd(10);   
+    insertAtEnd(20);   
+    insertAtEnd(30);   
+    insertAtEnd(40);   
+    insertAtEnd(50); 
+
+
+    cout<<"Before: "<<endl;
+    traverseNodes();
+
+    deleteANodeFromStart();
+    cout<<endl<<"After: "<<endl;
+    traverseNodes();
+
+}
