52c163675d9bb15eded530604a5ac2cce3cdf3bd
Daoud-Hussain <daoudhussain302@gmail.com>
2022-06-02 23:17:54 +0500

    Inserting nodes at the end of circular LL

---
+++ Circular Linkedlist/insertAtEnd.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+Node* first;
+Node* last;
+
+//Travesing/Printing nodes of linkedlist
+void traverseNodes(){
+    Node* temp = first;
+    while(temp->next != first){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    if(temp->next == first){
+        cout<<temp->data<<" ";
+
+    }
+}
+
+//Inserting a node at the start of linkedlist
+Node* insertAtEnd(int value){
+    if(first == NULL && last == NULL){
+        Node* curr = new Node();
+        curr->data = value;
+        curr->next = NULL;
+        first = curr;
+        last = curr;
+    }
+    else{
+        Node* curr = new Node();
+        curr->data = value;
+        curr->next = first;
+        last->next = curr;
+        last = curr;
+    }
+}
+
+
+int main(){
+    insertAtEnd(10);
+    insertAtEnd(20);
+    insertAtEnd(30);
+    insertAtEnd(40);
+    insertAtEnd(50);
+
+    traverseNodes();
+    cout<<endl<<"First Node: "<<first->data;
+    cout<<endl<<"Last Node: "<<last->data;
+}
