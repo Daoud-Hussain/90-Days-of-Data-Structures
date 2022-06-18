834a851f32ab946a3a1ea6fabefd9ae1ae8ab79d
Daoud-Hussain <daoudhussain302@gmail.com>
2022-06-18 13:43:15 +0500

    Sorting the array using mergeSort algo

---
+++ Sorting Algos/mergeSort.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+// Function to merge two subarrays L and M into arr
+void merge(int arr[], int p, int q, int r) {
+  
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+
+  // Until we reach either end of either L or M, pick larger among
+  // elements L and M and place them in the correct position at A[p..r]
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+// Divide the array into two subarrays, sort them and merge them
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    //Finding mid point of array in m
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    merge(arr, l, m, r);
+  }
+}
+
+// Print the array
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+    
+}
+
+int main()
+{   
+    int size = 10;
+    int arr[] = {10,21,2,30,5,13,44,17,9,31};
+
+    cout<<"Before"<<endl;
+    printArray(arr, size);
+
+    cout<<"\nAfter"<<endl;
+    mergeSort(arr, 0, size-1);
+    printArray(arr, size);
+
+}
