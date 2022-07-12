13f44e3e1a210f0e7bb10cc6d7ff5d08ecb1cea0
Daoud-Hussain <daoudhussain302@gmail.com>
2022-07-12 21:08:33 +0500

    Implementing stack using queue

---
+++ Queue/convertQueueToStack.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+/*
+    Problem Statement: A Queue deque data in the order of FIFO whereas, stack use LIFO.
+    Write a program that enqueue data like queue and dequeue like stack. i.e, dequeue using
+    LIFO.  
+*/
+
+struct Queue{
+    int front = -1;
+    int rear = -1;
+    int size = 10;
+    int* arr = new int[size];
+
+
+    bool isEmpty(){
+        if(front == -1 && rear == -1){
+            return true;
+        }
+        return false;
+    }
+
+    bool isFull(){
+        if(rear == size - 1){
+            return true;
+        }
+        return false;
+    }
+
+    void enQueue(int data){
+        if(isFull()){
+            cout<<"Queue Overflow"<<endl;
+        }
+        else if(isEmpty()){
+            front++;
+            rear++;
+            arr[rear] = data;
+        }
+        else{
+            rear++;
+            arr[rear] = data;
+        }
+    }
+
+    int deQueue(){
+        if(isEmpty()){
+            cout<<"Queue UnderFLow"<<endl;
+        }
+        else{
+            int value = arr[rear];
+            rear--;
+            return value;
+        }
+    }
+    
+
+    void display(){
+        int fr = front;
+        while(fr != rear){
+            cout<<arr[fr]<<" ";
+            fr++;
+        }
+        if(fr == rear){
+            cout<<arr[fr]<<" ";
+        }
+    }
+
+
+
+
+};
+
+int main()
+{
+    Queue myQueue;
+    myQueue.enQueue(10);
+    myQueue.enQueue(20);
+    myQueue.enQueue(30);
+    myQueue.enQueue(40);
+    myQueue.enQueue(50);
+    myQueue.enQueue(70);
+    myQueue.enQueue(80);
+
+    cout<<"Before"<<endl;
+    myQueue.display();
+    cout<<endl;
+    cout<<"After"<<endl;
+    myQueue.deQueue();
+    myQueue.deQueue();
+    myQueue.display();
+
+
+
+
+}
